90a93e021bba28405bcc787bf0648160933167b7
MarcusB86 <marcusb08@gmail.com>
2024-10-07 20:22:31 -0400

    404 page rendered without image

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 
 export default function NotFoundPage() {
   return (
-    <div>NotFound</div>
-  )
+    <div className="flex flex-col items-center justify-center min-h-screen bg-primaryLighter">
+      <div className="text-center">
+        <h1 className="text-9xl font-roboto text-dark relative">
+          404
+          <span className="absolute -top-20 w-full flex items-center justify-center">
+            <span className="text-5xl text-gray-700">oops!</span>
+          </span>
+        </h1>
+        <div className="not-found-text mt-10">
+          <p className="text-4xl font-sans text-white">This page was not found</p>
+          <p className="text-md mt-5 font-sans text-grayLight">You may have mistyped the address or this page may have moved</p>
+        </div>
+        <div className="Link-back-home mt-10">
+        <a className="text-white font-roboto hover: underline text-lg cursor-not-allowed">
+            Take me to Home page
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,14 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				purplePrimary: "#A594F9",
-				purpleLight: "#CDC1FF",
-				purpleLighter: "#F5EFFF",
-				purplePastel: "#E5D9F2",
-				grayLight: "#F6F5F5",
-				white: "#FFFFFF",
+				primary: "#1985A1",
+                primaryLighter: "#289dbc",
+                secondary: "#4C5C68",
+                secondaryLighter: "#657581",
+                dark: "#46494C",
+                grayDark: "#C5C3C6",
+                grayLight: "#DCDCDD",
+                white: "#FFFFFF",
 			},
 			boxShadow: {
 				'custom': 'rgba(0, 0, 0, 0.16) 0px 1px 4px',
