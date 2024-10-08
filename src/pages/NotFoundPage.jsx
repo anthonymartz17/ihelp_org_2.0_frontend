85771b7ef81795836047ce6b912b600e5f4db4ee
MarcusB86 <marcusb08@gmail.com>
2024-10-07 21:23:43 -0400

    Not found page image rendered sans functionality

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -4,18 +4,19 @@ export default function NotFoundPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-primaryLighter">
       <div className="text-center">
-        <h1 className="text-9xl font-roboto text-dark relative">
-          404
-          <span className="absolute -top-20 w-full flex items-center justify-center">
-            <span className="text-5xl text-gray-700">oops!</span>
-          </span>
-        </h1>
-        <div className="not-found-text mt-10">
-          <p className="text-4xl font-sans text-white">This page was not found</p>
-          <p className="text-md mt-5 font-sans text-grayLight">You may have mistyped the address or this page may have moved</p>
+      <div className="flex justify-center">
+        <img 
+          src='src/assets/graphics/404 not found.png'
+          alt="Not Found Page" 
+          className="w-3/4 lg:w-3/4" 
+        />
         </div>
-        <div className="Link-back-home mt-10">
-        <a className="text-white font-roboto hover: underline text-lg cursor-not-allowed">
+        <div className="not-found-text">
+          <p className="text-4xl font-sans text-white font-bold">This page was not found</p>
+          <p className="text-md mt-5  font-sans text-grayLight">You may have mistyped the address or this page may have moved</p>
+        </div>
+        <div className="Link-back-home mt-5">
+        <a className="text-white font-roboto font-bold hover: underline text-lg cursor-pointer">
             Take me to Home page
           </a>
         </div>
