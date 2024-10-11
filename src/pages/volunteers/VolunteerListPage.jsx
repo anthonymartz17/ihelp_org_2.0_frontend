2142690288781a82f9ed07ef4b4dc41cb68a3196
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-10-11 08:26:55 -0400

    format volunteer folder

--- src/pages/volunteers/VolunteerListPage.jsx
+++ src/pages/volunteers/VolunteerListPage.jsx
@@ -1,7 +1,7 @@
 export default function VolunteerListPage() {
-	return (
-		<div className="flex flex-col gap-4 py-3 pl-8">
-			<h1 className="roboto-bold text-xl">Volunteers</h1>
-		</div>
-	);
+  return (
+    <div className="flex flex-col gap-4 py-3 pl-8">
+      <h1 className="roboto-bold text-xl">Volunteers</h1>
+    </div>
+  );
 }
--- src/pages/volunteers/VolunteerNewPage.jsx
+++ src/pages/volunteers/VolunteerNewPage.jsx
@@ -3,9 +3,10 @@ import VolunteersForm from "../../components/forms/VolunteersForm";
 export default function VolunteerNewPage() {
   return (
     <div>
-      <h1 className="text-2xl text-dark ml-10 font-bold text-left mb-6">New Volunteer</h1>
+      <h1 className="text-2xl text-dark ml-10 font-bold text-left mb-6">
+        New Volunteer
+      </h1>
       <VolunteersForm />
-      </div>
-  )
+    </div>
+  );
 }
-
