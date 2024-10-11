89d2c9e20b7108465a1ebb40e6cec1c795a09d84
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-10-11 08:28:02 -0400

    requests folder formatted

--- src/pages/requests/RequestEditPage.jsx
+++ src/pages/requests/RequestEditPage.jsx
@@ -1,10 +1,10 @@
 import RequestForm from "../../components/forms/RequestForm";
 
 export default function RequestEditPage() {
-	return (
-		<div>
-			RequestEditPage
-			<RequestForm />
-		</div>
-	);
+  return (
+    <div>
+      RequestEditPage
+      <RequestForm />
+    </div>
+  );
 }
