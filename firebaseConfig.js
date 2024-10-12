682b6a797f046ab797933ccf44f6091843be02ca
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-10-12 10:51:07 -0400

    import fix

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APPID,
+  apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
   projectId: import.meta.env.VITE_PROJECTID,
   storageBucket: import.meta.env.VITE_STROAGEBUCKET,
