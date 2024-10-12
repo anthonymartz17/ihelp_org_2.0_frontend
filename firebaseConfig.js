9561d6f205cda81ca4a84a9babfd08726207a25b
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-10-12 10:48:09 -0400

    moved imports

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8FA5PoXN3oVg0s5S3sEeiV92sMqzDfv8",
-  authDomain: "ihelp-81806.firebaseapp.com",
-  projectId: "ihelp-81806",
-  storageBucket: "ihelp-81806.appspot.com",
-  messagingSenderId: "689396817516",
-  appId: "1:689396817516:web:47b1c792a0064fdf9c8249",
-  measurementId: "G-RHDYCV2LBL",
+  apiKey: import.meta.env.VITE_APPID,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STROAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESASAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID,
 };
 
 const app = initializeApp(firebaseConfig);
