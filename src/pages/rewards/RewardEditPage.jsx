6773c27c3a1272257ca648d74b80dc922b95f480
MarcusB86 <marcusb08@gmail.com>
2024-10-20 15:29:00 -0400

    rewardsForm completed and rendered to EDIT/NEW

--- src/pages/rewards/RewardEditPage.jsx
+++ src/pages/rewards/RewardEditPage.jsx
@@ -3,7 +3,9 @@ import RewardForm from "../../components/forms/RewardsForm";
 export default function RewardEditPage() {
   return (
     <div>
+      <h1 className="text-2xl text-dark ml-10 font-bold text-left mb-6">
       RewardEditPage
+      </h1>
       <RewardForm />
       </div>
     );
