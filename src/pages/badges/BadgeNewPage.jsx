3a50732121bc32986d1d3f3fdb5cb302236c9d91
MarcusB86 <marcusb08@gmail.com>
2024-10-22 20:01:52 -0400

    Badgesform complete and rendered to Edit and New

--- src/pages/badges/BadgeNewPage.jsx
+++ src/pages/badges/BadgeNewPage.jsx
@@ -1,3 +1,13 @@
+import BadgesForm from "../../components/forms/BadgesForm";
+
+
 export default function BadgesNewPage() {
-  return <div>BadgesNewPage</div>;
+  return (
+  <div>
+    <h1 className="text-2xl text-dark ml-10 font-bold text-left mb-6">
+    BadgesNewPage
+     </h1>
+    <BadgesForm />
+    </div>
+  );
 }
