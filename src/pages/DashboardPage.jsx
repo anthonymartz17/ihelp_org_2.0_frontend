6ce7e7eff994364fbb31bc6536aef6b32c1d3145
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-11-02 13:22:15 -0400

    sidebar fixed

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -6,14 +6,14 @@ import Sidebar from "../components/Sidebar";
 export default function DashboardPage() {
   return (
     <div className="flex h-screen bg-white ">
-      <aside className="w-1/6 bg-primary shadow-custom text-white">
+      <aside className="h-screen sticky top-0 w-1/6 bg-primary shadow-custom text-white">
         <Sidebar />
       </aside>
 
       <div className="flex flex-col  h-full w-full">
         <Header />
 
-        <main>
+        <main className="flex-grow overflow-y-auto p-4">
           <Outlet />
         </main>
       </div>
